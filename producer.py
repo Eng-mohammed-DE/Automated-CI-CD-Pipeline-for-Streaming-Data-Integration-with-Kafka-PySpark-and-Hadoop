05661003d5c9e9c211075f011cf1158397a49a48
mohammed <eng.mohammed.data.enginner@gmail.com>
2025-04-14 07:15:01 +0200

    Updated topic in producer

--- producer.py
+++ producer.py
@@ -4,11 +4,11 @@ import time
 
 # Kafka Producer Configuration
 producer = KafkaProducer(bootstrap_servers='192.168.1.39:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-request_timeout_ms=120000 
 
-# Infinite loop to send data to Kafka
+topic = 'node101' 
+
 while True:
     message = {"sensor_id": "123", "temperature": 25}
-    producer.send('node101', message)
-    print(f"Sent: {message}")
-    time.sleep(2)  # Simulate sending data every 2 seconds
+    producer.send(topic, message)
+    print(f"Sent to {topic}: {message}")
+    time.sleep(2)
