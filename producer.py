a26a0afa6c6c06e5421887c687ebca8fdd44b14a
mohammed <eng.mohammed.data.enginner@gmail.com>
2025-04-14 05:19:15 +0200

    Updated ip

--- producer.py
+++ producer.py
@@ -4,6 +4,7 @@ import time
 
 # Kafka Producer Configuration
 producer = KafkaProducer(bootstrap_servers='192.168.1.39:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+request_timeout_ms=120000 
 
 # Infinite loop to send data to Kafka
 while True:
